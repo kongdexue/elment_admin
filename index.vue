d9dfbdc479e77d69d8d0a385ec2aefb01fead2ec
admin <723684067@qq.com>
2017-12-17 13:27:29 +0800

    上传

---
+++ index.vue
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <!-- 引入样式 -->
+  <link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">
+</head>
+<body>
+<div id="app">
+<el-container style="height: 500px; border: 1px solid #eee">
+  <el-aside width="200px" style="border: 1px solid #eee">2
+    <el-menu>s</el-menu>
+  </el-aside>
+  <el-container>
+    <el-header style="border: 1px solid #eee">s</el-header>
+    <el-main style="border: 1px solid #eee">ddd</el-main>
+  </el-container>
+</el-container>
+</div>
+</body>
+  <!-- 先引入 Vue -->
+  <script src="https://unpkg.com/vue/dist/vue.js"></script>
+  <!-- 引入组件库 -->
+  <script src="https://unpkg.com/element-ui/lib/index.js"></script>
+  <script>
+    new Vue({
+      el: '#app',
+      data: {
+        username : '',
+        password : '',
+        isload:false
+      },
+       methods: {
+            open:function() {
+                this.isload=true;
+                setTimeout(function(){
+                this.$alert('登录成功', '登录中');
+                this.isload=false;
+                 }.bind(this),2000)
+            }
+        }
+    })
+  </script>
+</html>
---
+++ login.html
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <!-- 引入样式 -->
+  <link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">
+</head>
+<body>
+<div id="app">
+<el-input v-model="username" placeholder="username"></el-input>
+<el-input v-model="password" placeholder="password" type="password"></el-input>
+<el-button type="primary" :loading="isload" @click="open">login</el-button>
+</div>
+</body>
+  <!-- 先引入 Vue -->
+  <script src="https://unpkg.com/vue/dist/vue.js"></script>
+  <!-- 引入组件库 -->
+  <script src="https://unpkg.com/element-ui/lib/index.js"></script>
+  <script>
+    new Vue({
+      el: '#app',
+      data: {
+        username : '',
+        password : '',
+        isload:false
+      },
+       methods: {
+            open:function() {
+                this.isload=true;
+                setTimeout(function(){
+                this.$alert('登录成功', '登录中');
+                this.isload=false;
+                 }.bind(this),2000)
+            }
+        }
+    })
+  </script>
+</html>
